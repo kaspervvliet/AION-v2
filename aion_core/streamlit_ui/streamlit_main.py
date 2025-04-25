4121070a4565db678970f936b50d9a1d33c0576b
kaspervvliet <kaspervvliet@gmail.com>
2025-04-25 14:11:09 +0200

    Delete aion_core/streamlit_ui/streamlit_main.py

--- aion_core/streamlit_ui/streamlit_main.py
+++
@@ -1,6 +0,0 @@
-
-import streamlit as st
-from aion_core.streamlit_ui import dashboard
-
-# Hoofdbestand voor run: `streamlit run aion_core/streamlit_ui/streamlit_main.py`
-st.switch_page("aion_core/streamlit_ui/dashboard.py")
