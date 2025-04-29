0cbb2fb5b87677c910b43e1039ae45bd2c7854f2
kaspervvliet <kaspervvliet@gmail.com>
2025-04-29 22:35:55 +0200

    Add files via upload

---
+++ main_backtest.py
@@ -0,0 +1,4 @@
+from aion_core.context.context_manager import ContextManager
+from aion_core.signals.signal_manager import SignalManager
+from aion_core.signals.signal_executor import execute_signal
+# main_backtest.py - fixed imports to strategy_pool_initializer
