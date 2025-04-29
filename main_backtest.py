ea6c3037d7aaab8a33ca9b0eacabd85c4f439d68
kaspervvliet <kaspervvliet@gmail.com>
2025-04-29 23:13:30 +0200

    Delete main_backtest.py

--- main_backtest.py
+++
@@ -1,4 +0,0 @@
-from aion_core.context.context_manager import ContextManager
-from aion_core.signals.signal_manager import SignalManager
-from aion_core.signals.signal_executor import execute_signal
-# main_backtest.py - fixed imports to strategy_pool_initializer
