cd3144568b9ffd8fc6c0021cd8a9f2c51c95a2c0
kaspervvliet <kaspervvliet@gmail.com>
2025-04-30 23:01:52 +0200

    Delete aion_core/__init__.py

--- aion_core/__init__.py
+++
@@ -1,7 +0,0 @@
-from aion_core.core.logging_engine import (
-    handle_context_logging,
-    handle_trade_result_logging,
-    handle_bias_update_logging,
-    handle_equity_update_logging,
-    handle_reflection_logging
-)
