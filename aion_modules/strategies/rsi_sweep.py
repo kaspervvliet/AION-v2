1023c3da308aff58ea62080e5e22b85fd839619c
kaspervvliet <kaspervvliet@gmail.com>
2025-05-01 18:07:17 +0200

    Delete aion_modules/strategies directory

--- aion_modules/strategies/rsi_sweep.py
+++
@@ -1,8 +0,0 @@
-from aion_core.ai.strategy_interface import StrategyInterface
-
-class RsiSweep(StrategyInterface):
-    name = "rsi_sweep"
-
-    def validate_market(self, recent_logs):
-        # TODO: vervang dit met echte validatielogica
-        return True
