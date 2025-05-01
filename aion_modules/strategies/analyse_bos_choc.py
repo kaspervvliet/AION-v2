44d764ebc577ac3247b132e9b83f58b5edf821c6
kaspervvliet <kaspervvliet@gmail.com>
2025-05-01 18:08:01 +0200

    Add files via upload

---
+++ aion_modules/strategies/analyse_bos_choc.py
@@ -0,0 +1,8 @@
+from aion_core.ai.strategy_interface import StrategyInterface
+
+class AnalyseBosChoc(StrategyInterface):
+    name = "analyse_bos_choc"
+
+    def validate_market(self, recent_logs):
+        # TODO: vervang dit met echte validatielogica
+        return True
